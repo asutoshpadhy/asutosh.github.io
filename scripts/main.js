2566a43deb8d6d659de289d536cf64af0b139cc8
Asutosh Padhy <44151108+asutoshpadhy@users.noreply.github.com>
2019-01-22 15:00:48 +0530

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -154,3 +154,16 @@ $(document).ready(function(){
         $("#form2").toggle();
     });
 });
+
+
+
+   function disablefirstbutton() {
+    document.getElementById("firstbutton").disabled = true;
+    document.getElementById("secondbutton").disabled = false;
+}
+
+function disablesecondbutton() {
+    document.getElementById("secondbutton").disabled = true;
+    document.getElementById("firstbutton").disabled = false;
+}
+
